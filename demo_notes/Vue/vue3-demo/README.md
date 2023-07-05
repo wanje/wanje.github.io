7f524b8c4623e1c685a882178bd8d2f5717bb53d
Jason <wanjeay@gmail.com>
2023-07-05 17:50:09 +0800

    vue3-demo

--- demo_notes/Vue/vue3-demo/README.md
+++ demo_notes/Vue/vue3-demo/README.md
@@ -1,29 +1,11 @@
-### 使用vite构建工具创建vue3项目
+# 创建基于vite构建工具的vue3项目
 
-命令行中两个杠"--"的表示指令，一个杠"-"的表示参数
+[Vite配置文档](https://cn.vitejs.dev/config/)。
 
-```shell
- npm install vue-cli -g
+执行以下命名，其将会安装并执行`create-vue`（Vue官方的项目脚手架工具），然后根据提示操作即可
+```sh
+# 目前 @latest 就是 vue@3 最新版本
+npm init vue@latest
+
+# 项目创建完成后进入项目目录，安装依赖后即可启动
 ```
-* 参数 -g 表示全局安装，安装后以后都能用，不必每个项目再单独安装，可以通过 vue -V 命令判断是否安装vue-cli
-
-```shell
- vue init webpack projectName
-```
-* 根据模板初始化一个vue项目，webpack是模板名(可使用官方提供的其他模板，也可以自定义模板custom)，projectName是创建的该vue项目的名字；
-* 执行该命令时会提示选择或输入该项目的相关信息及是否使用一些代码检测等辅助工具，安装完成后会提示后面你需要做的三步(即下面3步)
-
-```shell
- cd projectName
-```
-* 进入刚刚创建的项目文件夹，项目文件夹是自动生成在上面模板初始化命令所在执行位置的，所以想在哪里创建项目，就要在哪个文件夹下执行步骤2的命令
-
-```shell
- npm install
-```
-* 该命令是安装默认的一些依赖包，不然工程运行不起来，这里默认安装的是开发环境依赖，若只安装生产环境依赖则后面要加指令 --production，对应则安装"dependencies"下的依赖
-
-```shell
- npm run dev
-```
-* 该命令就是以开发模式运行该vue项目(同npm start)，npm run build则是打包压缩(生产模式)，其实执行的都是package.json文件中"scripts"配置下的命令(最终指向一些nodejs)
---
+++ demo_notes/Vue/vue3-demo/vite.config.js
@@ -0,0 +1,73 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+
+// 自动导入组件/工具库的API，使无需手动在头部import并注册即可用
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // base: './',  // 资源基础路径，空字符串或'./'为相对路径，默认根路径'/'
+  publicDir: 'public',  // 静态资源目录位置，默认`public`，此目录中的内容不会经过vite处理，打包时会直接拷贝到设置的outDir目录，且访问时路径已`/`开始
+  plugins: [
+    vue(),
+    vueJsx(),
+    AutoImport({
+      imports: ['vue', 'vue-router', 'pinia'],
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      // dirs: ['src/components'], // 可自动查找导入并注册相关自定义组件的目录(相对路径，默认`src/components`目录，类似nuxt.js中自定导入注册该目录下组件)
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    devSourcemap: true, // 开发环境下开启css sourceMap
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/styles/mixins.scss";' // scss全局注入
+      }
+    }
+  },
+  server: {
+    // base: '',  // http请求的基础前置路径
+    host: true, // 此时将同时监听局域网和公网，可用于使用ip形式访问，默认 localhost
+    // port: 8080, // 自定义端口号，默认 5173
+    open: true,
+    proxy: {
+      // 字符串简写写法：http://localhost:5173/bd_finance -> https://finance.pae.baidu.com/bd_finance
+      '/bd_finance': 'https://finance.pae.baidu.com',
+      // 选项写法
+      '/bd_gushitong': {
+        target: 'https://gushitong.baidu.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/bd_gushitong/, '')  // 重写部分路径
+      },
+      // 正则写法
+      '^/bd_opendata': {
+        target: 'http://opendata.baidu.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/bd_opendata/, '')
+      },
+      // 代理 websockets 或 socket.io 写法：ws://localhost:5173/socket.io -> ws://localhost:5174/socket.io
+      '/socket.io': {
+        target: 'ws://localhost:5174',
+        ws: true, // 是否代理websockets
+      },
+    }
+  },
+  build: {
+    // outDir: 'dist',  // 指定打包输出路径，默认‘dist’
+    // assetsDir: 'assets',  // 打包后静态资源的存放路径（相对上面的`outDir`），默认`assets`
+    assetsInlineLimit: 4096,  // 静态资源内联限制大小(字节)，不超过此大小的图片等静态资源将转为base64内联，默认`4096`即`4kb`
+  }
+})
